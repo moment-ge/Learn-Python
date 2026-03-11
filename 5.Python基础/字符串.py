42496f8f3557ab86f874fd82ee08030fe78ebeaf
moment-ge <18555156639@163.com>
2026-03-11 17:23:50 +0800

    feat:数据类型和变量

---
+++ 5.Python基础/字符串.py
@@ -0,0 +1,15 @@
+# 换行
+print("Hello\nWorld")
+
+# 制表符
+print("Hello\tWorld")
+
+# 反斜杠
+print("Backslash: \\")
+
+
+# 使用三引号创建多行字符串
+multiline_string = '''Hello,
+This is a multi-line
+string.'''
+print(multiline_string)
